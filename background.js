73987d70122942a58163cc0ce0db5cc84334fa07
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2022-11-30 01:17:18 +0100

    feat: updated to manifest v3, fixed error All error should be fixed now. Did some testing with the declarativeNetRequests and the ruleset. Reimplemented logging for requests, but limited to the currently active tab now.

--- background.js
+++ background.js
@@ -1,7 +1,35 @@
-chrome.webRequest.onCompleted.addListener(
-    function(details) {
-        console.log(details);
-    },
-    {urls: ["<all_urls>"]},
-    ["responseHeaders"]
-);
+async function getCurrentTab() {
+	let queryOptions = { active: true, lastFocusedWindow: true };
+	// `tab` will either be a `tabs.Tab` instance or `undefined`.
+	let [tab] = await chrome.tabs.query(queryOptions);
+	return tab;
+}
+
+const addWebRequestListener = (url) => {
+	chrome.webRequest.onCompleted.addListener(
+		function (details) {
+			console.log(`New request from ${details.initiator}`);
+			console.log(details);
+		},
+		// { urls: ['<all_urls>'] },
+		{ urls: [`*://${url}/*`] },
+		['responseHeaders']
+	);
+};
+
+const onChangeTab = async (activeInfo) => {
+	const { id, url, title, favIconUrl } = await getCurrentTab();
+	if (!url) return; // Early return if url is undefined (happens on opening a new tab)
+	const { hostname } = new URL(url);
+
+	console.log('Tab changed');
+	console.log(`Current url: ${url}`);
+	console.log(`Hostname: ${hostname}`);
+	if (hostname) {
+		addWebRequestListener(hostname);
+	}
+};
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	onChangeTab(activeInfo);
+});
